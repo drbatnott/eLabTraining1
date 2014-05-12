b8fd8a35ffb735e53233c8184a72908a96a9c569
Peter Blanchfield <peter.blanchfield@nottingham.ac.uk>
2014-05-12 14:42:21 +0100

    Added Maven Project
    Project framework added

---
+++ eLabTraining/src/main/java/eLab/eLabTraining/App.java
@@ -0,0 +1,17 @@
+package eLab.eLabTraining;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public App(){
+		
+	}
+    public static void main( String[] args )
+    {
+    	App app = new App();
+        System.out.println( "Hello World!" );
+    }
+}
