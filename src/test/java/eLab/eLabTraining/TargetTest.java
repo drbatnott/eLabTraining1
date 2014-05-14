c0f167bd3b83d71d94ffced23d97d590eeb9244c
Selnabarawy <s.elnabarawy@eng.asu.edu.eg>
2014-05-14 13:34:06 +0200

    Created Target Test class

---
+++ src/test/java/eLab/eLabTraining/TargetTest.java
@@ -0,0 +1,12 @@
+package eLab.eLabTraining;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+public class TargetTest    extends TestCase {
+    public void testTargetWorks()
+    {
+    	Target t = new Target();
+        assertNotNull(t);
+    }
+
+}
