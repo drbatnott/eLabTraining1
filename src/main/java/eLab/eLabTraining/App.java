fdb8386e1d6cac13057aea53b25e16d5238f934a
Ali Zein <alizein@eng.asu.edu.eg>
2014-05-16 10:32:26 +0200

    Added some code to player and to main App to test player.

--- src/main/java/eLab/eLabTraining/App.java
+++ src/main/java/eLab/eLabTraining/App.java
@@ -1,17 +1,64 @@
 package eLab.eLabTraining;
 
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-	public App(){
+public class App extends JFrame implements KeyListener {
+
+	static Player fred;
+
+	public App() {
+		fred = new Player("fred");
+		add(fred.getCharacter());
+
+		setSize(600, 300);
+		show();
+		addKeyListener((KeyListener) this);
 		
 	}
-    public static void main( String[] args )
-    {
-    	App app = new App();
-        System.out.println( "Hello World w a7la messa 3al 7elween :D" );
-    }
+
+	public static void main(String[] args) {
+		App app = new App();
+		System.out.println("Hello World w a7la messa 3al 7elween :D");
+
+	}
+
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		Point currentLocation = fred.getCharacter().getLocation();
+		
+		if (arg0.getKeyCode() == KeyEvent.VK_UP) {
+			fred.moveUp(currentLocation);
+			repaint();
+		} else if (arg0.getKeyCode() == KeyEvent.VK_DOWN) {
+			fred.moveDown(currentLocation);
+			repaint();
+		} else if (arg0.getKeyCode() == KeyEvent.VK_RIGHT) {
+			fred.moveRight(currentLocation);
+			repaint();
+		} else if (arg0.getKeyCode() == KeyEvent.VK_LEFT) {
+			fred.moveLeft(currentLocation);
+			repaint();
+		}
+	}
+
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println("pressed");
+	}
+
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println("pressed");
+	}
+
 }
--- src/main/java/eLab/eLabTraining/Player.java
+++ src/main/java/eLab/eLabTraining/Player.java
@@ -1,9 +1,58 @@
 package eLab.eLabTraining;
 
-public class Player {
+import java.awt.Point;
+import java.io.CharConversionException;
 
-	public Player(String string) {
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class Player {
+	
+	String playerName;
+	String playerCharacter;
+	int playerScore;
+	int playerLevel;
+	int playerRemainingTime;
+	JLabel character;
+	int characterSpeed = 2;
+	
+	ImageIcon front = new ImageIcon("C:/Users/Ali Zein/Dropbox/Trainings/Nottingham Training/SprCre3 VX v20131202.1425/Saved/front_1.png");
+	ImageIcon back = new ImageIcon("C:/Users/Ali Zein/Dropbox/Trainings/Nottingham Training/SprCre3 VX v20131202.1425/Saved/back_1.png");
+	ImageIcon right = new ImageIcon("C:/Users/Ali Zein/Dropbox/Trainings/Nottingham Training/SprCre3 VX v20131202.1425/Saved/right_1.png");
+	ImageIcon left = new ImageIcon("C:/Users/Ali Zein/Dropbox/Trainings/Nottingham Training/SprCre3 VX v20131202.1425/Saved/left_1.png");
+
+	
+
+	public Player(String player) {
 		// TODO Auto-generated constructor stub
+		playerName = player;
+		character = new JLabel(front);
 	}
+	
+	public JLabel getCharacter() {
+		return character;
+	}
+	
+	public void moveRight(Point p) {
+		character.setIcon(right);
+		character.setLocation(p.x+characterSpeed, p.y);
+    }
+
+    public void moveLeft(Point p) {
+    	character.setIcon(left);
+    	character.setLocation(p.x-characterSpeed, p.y);
+    }
+
+    public void moveUp(Point p) {
+    	character.setIcon(back);
+    	character.setLocation(p.x, p.y-characterSpeed);
+    }
+
+    public void moveDown(Point p) {
+    	character.setIcon(front);
+    	character.setLocation(p.x, p.y+characterSpeed);
+    }
+	
+	
 
 }
